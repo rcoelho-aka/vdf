5bcec98aa3a94c50970539e5d61b67c378cab073
mlopes <miguel.lopes4@vodafone.com>
2023-01-11 18:10:46 +0000

    Miguel Lopes final commit

---
+++ MiguelLopes.java
@@ -0,0 +1,10 @@
+public class MiguelLopes {
+    public static void sayName() {
+        Student student = new Student(firstName:"Ricardo", lastName:"Coelho");
+        student.sayFirstName();
+        student.sayLastName();
+    }
+
+    public static void sayName() {
+    }
+}
