2979598984303f2128b23bc04d51383c03ff0314
amgalves96 <amg.alves@campus.fct.unl.pt>
2023-01-11 17:28:04 +0000

    added file AfonsoAlves.java

---
+++ AfonsoAlves.java
@@ -0,0 +1,7 @@
+public class AfonsoAlves {
+    public static void sayName() {
+        Student student = new Student("Afonso", "Alves");
+        student.sayFirstName();
+        student.sayLastName();
+    }
+}
