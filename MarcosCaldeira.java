26142c33652d37f96397359337c69d3193627e0f
MarcosSWE <marcoshsec1992@gmail.com>
2023-01-11 17:44:47 +0000

    Add MarcosCaldeira.java & Change Main.java

---
+++ MarcosCaldeira.java
@@ -0,0 +1,7 @@
+public class MarcosCaldeira {
+	    public static void sayName() {
+		            Student student = new Student("Marcos", "Caldeira");
+			            student.sayFirstName();
+				            student.sayLastName();
+					        }
+}
