2ca87b11aa841811199305266f43d4a61339ba9f
Vasco Baptista <Vasco.Batista@vodafone.com>
2023-01-11 17:54:53 +0000

    Alterado a class VascoBatista

--- VascoBatista.java
+++ VascoBatista.java
@@ -1,8 +1,11 @@
 public class VascoBatista {
     public static void sayName() {
-        Student student = new Student("Vasco", "Baptista");
+        Student student = new Student("Vasco ", "Baptista");
+        System.out.print("My name is ");
         student.sayFirstName();
+        System.out.print(" ");
         student.sayLastName();
+        System.out.print("!");
     }
 
 
