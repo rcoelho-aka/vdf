584f2f2c149489acad5ed61b1e0bc07b44ed08ef
fdguerreiro <filipedcguerreiro@gmail.com>
2023-01-11 18:00:26 +0000

    alteração 4

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        FilipeGuerreiro nome =  new FilipeGuerreiro("Filipe", "Guerreiro");
-
         FilipeGuerreiro.sayName();
 
     }
