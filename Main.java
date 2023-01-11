58dfd9ac0cca7e4bed942a01c1c9a6a575381c50
Joao Quintaneiro <joao.quintaneiro@vodafone.com>
2023-01-11 17:48:01 +0000

    Step4 - Merged main , forgot main

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-<<<<<<< HEAD
         JoaoQuintaneiro.sayName();
-=======
         RicardoCoelho.sayName();
         MartimViana.sayName();
         AfonsoAlves.sayName();
         PauloVargas.sayName();
         AntonioSantos.sayName();
->>>>>>> main
     }
 }
