3e9bf781d8ed227335a214df42ae1b4decb7ffc2
MartimViana <martimviana98@gmail.com>
2023-01-11 17:30:57 +0000

    Fix MartimViana.java to have described behaviour

--- MartimViana.java
+++ MartimViana.java
@@ -1,5 +1,25 @@
 public class MartimViana {
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void sayFirstName() {
+        System.out.print(firstName);
+    }
+
+    public void sayLastName() {
+        System.out.print(lastName);
+    }
+
     public static void sayName() {
-        System.out.println("My name is Martim Viana!");
+        System.out.print("My name is ");
+        this.sayFirstName();
+        System.out.print(" ");
+        this.sayLastName();
+        System.out.print("!");
     }
 }
