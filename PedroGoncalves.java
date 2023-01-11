f214700a6278a72feb7cb33d00c614620e25743b
pedromlg6 <pedro.goncalves@vodafone.com>
2023-01-11 18:12:03 +0000

    pedrogon

---
+++ PedroGoncalves.java
@@ -0,0 +1,7 @@
+public class PedroGoncalves {
+    public static void sayName() {
+        Student student = new Student("Pedro", "Goncalves");
+        student.sayFirstName();
+        student.sayLastName();
+    }
+}
