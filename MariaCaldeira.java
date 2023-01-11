ba4c4355d3e6499cb4e275aa96dc4d3a06153a89
MariaCaldeira99 <mariagcaldeira99@gmail.com>
2023-01-11 17:35:02 +0000

    Added new output

--- MariaCaldeira.java
+++ MariaCaldeira.java
@@ -1,7 +1,11 @@
 public class MariaCaldeira {
     public static void sayName() {
         Student student = new Student("Maria", "Caldeira");
+        
+        System.out.print("My name is ");
         student.sayFirstName();
+        System.out.print(" ");
         student.sayLastName();
+        System.out.print("!");
     }
 }
