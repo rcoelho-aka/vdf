050a5c7034e7cad76ba633aede4a331a34e83f3e
pedromlg6 <pedro.goncalves@vodafone.com>
2023-01-11 18:52:30 +0000

    My name is Pedro Goncalves!

--- PedroGoncalves.java
+++ PedroGoncalves.java
@@ -1,7 +1,10 @@
 public class PedroGoncalves {
     public static void sayName() {
         Student student = new Student("Pedro", "Goncalves");
+        System.out.print("My name is ");
         student.sayFirstName();
+        System.out.print(" ");
         student.sayLastName();
+        System.out.print("!");
     }
 }
