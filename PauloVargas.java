dd68b9352cc8d8b6cfcb8c9794dfefe234463479
paulovargasvf <paulo.vargas2@vodafone.com>
2023-01-11 15:49:08 +0000

    Created PauloVargas.java and added function to call it in Main.java

---
+++ PauloVargas.java
@@ -0,0 +1,8 @@
+
+public class PauloVargas {
+    public static void sayName() {
+        Student student = new Student("Paulo", "Vargas");
+        student.sayFirstName();
+        student.sayLastName();
+    }
+}
