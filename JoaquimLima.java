b6d8232a27fc31d9ddc24d105cd179e1af0e6e53
limaj <limaj93@gmail.com>
2023-01-11 18:15:15 +0000

    JoaquimLima

--- JoaquimLima.java
+++ JoaquimLima.java
@@ -1,2 +1,10 @@
-Hello World!
-JoaquimLima
+public class JoaquimLima {
+    public static void sayName() {
+        Student student = new Student("Joaquim", "Lima");
+        System.out.print("Hello My Name is ");
+        student.sayFirstName();
+        System.out.print(" ");
+        student.sayLastName();
+        System.out.print("!");
+    }
+}
