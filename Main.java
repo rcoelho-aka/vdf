9d7f1cfa3c40996d679f29e03b3da1cec4d9abed
Vasco Baptista <Vasco.Batista@vodafone.com>
2023-01-11 18:16:46 +0000

    Resolve conflitos Vasco Batista

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        AndreMatias.sayName();
+        JoaoQuintaneiro.sayName();
+        FilipaNunes.sayName();
+        MariaCaldeira.sayName();
+        RicardoCoelho.sayName();
+        MartimViana.sayName();
+        AfonsoAlves.sayName();
+        FilipeGuerreiro.sayName();
         VascoBatista.sayName();
-
-    
     }
 }
