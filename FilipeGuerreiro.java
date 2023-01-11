cf4136a80212df97163920382e17ae3b5b86f243
fdguerreiro <filipedcguerreiro@gmail.com>
2023-01-11 17:36:48 +0000

    Alteração ao Java File

---
+++ FilipeGuerreiro.java
@@ -0,0 +1,22 @@
+public class FilipeGuerreiro {
+    String firstName = "Filipe";
+    String lastName = "Guerreiro";
+
+    public FilipeGuerreiro(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void sayFirstName() {
+        System.out.print(firstName);
+    }
+
+    public void sayLastName() {
+        System.out.print(lastName);
+    }
+
+    public void sayName(){
+        System.out.println(firstName + " " + lastName);
+    }
+
+}
