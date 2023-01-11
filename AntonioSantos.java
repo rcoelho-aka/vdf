8f38fd2a2a25e66668194c028f69695e33d07176
AntonioJVSantos <Antonio-j.santos@vodafone.com>
2023-01-11 17:17:36 +0000

    Modified Antonio Santos added !

--- AntonioSantos.java
+++ AntonioSantos.java
@@ -2,9 +2,10 @@
 public class AntonioSantos {
     public static void sayName() {
         Student s = new Student("Antonio","Santos");
-        System.out.print(" Hello My Name is ");
+        System.out.print("Hello My Name is ");
         s.sayFirstName();
         System.out.print(" ");
         s.sayLastName();
+        System.out.print("!");
     }
 }
