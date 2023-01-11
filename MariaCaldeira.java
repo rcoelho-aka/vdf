aae49cc8bce25ee98d9c1e5abef4768f41403f68
MariaCaldeira99 <mariagcaldeira99@gmail.com>
2023-01-11 17:27:01 +0000

    Added MariaCaldeira.java

---
+++ MariaCaldeira.java
@@ -0,0 +1,7 @@
+public class MariaCaldeira {
+    public static void sayName() {
+        Student student = new Student("Maria", "Caldeira");
+        student.sayFirstName();
+        student.sayLastName();
+    }
+}
