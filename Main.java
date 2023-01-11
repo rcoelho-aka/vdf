a84ab5e12bde361d0e436428b08f837223b5cada
amgalves96 <amg.alves@campus.fct.unl.pt>
2023-01-11 17:41:35 +0000

    deleted unwanted files

--- Main.java
+++ Main.java
@@ -2,7 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         AfonsoAlves.sayName();
-        PauloVargas.sayName();
-        AntonioSantos.sayName();
     }
 }
