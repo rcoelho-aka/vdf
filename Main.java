e65c2f4bd55659436ed576ba6b71edf7bf3c7b44
JMarques <joao-a.marques@vodafone.com>
2023-01-11 18:37:56 +0000

    Modified main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        JoaoMarques showName = new JoaoMarques();
+
+        System.out.println("My name is: " + showName);
+
     }
 }
