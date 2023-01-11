eb8c3159174c647353395e0013679c0a26d031d6
Joao Quintaneiro <joao.quintaneiro@vodafone.com>
2023-01-11 17:40:25 +0000

    Step3 - Changed my code to print the entire phrase

--- JoaoQuintaneiro.java
+++ JoaoQuintaneiro.java
@@ -1,7 +1,10 @@
 public class JoaoQuintaneiro {
     public static void sayName() {
         Student student = new Student("João", "Quintaneiro");
+        System.out.print("Hello My Name is ");
         student.sayFirstName();
+        System.out.print(" ");
         student.sayLastName();
+        System.out.print("!");
     }
 }
