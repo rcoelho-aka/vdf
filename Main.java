ca9af2ca9a44451b7871056293660e6404015bfc
Afonso Alves <102078142+amgalves96@users.noreply.github.com>
2023-01-11 19:03:43 +0000

    removed extra space

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        DanielDias.sayName();
+          DanielDias.sayName();
         PauloVargas.sayName();
         AfonsoAlves.sayName();
         AndreMatias.sayName();
@@ -16,8 +16,8 @@ public class Main {
         VascoBatista.sayName();
         ViniciusLuz.sayName();
         AntonioSantos.sayName();
+	MiguelLopes.sayName();
         TomasRodrigues.sayName();
-        IsabelSoares.sayName();
-	      MiguelLopes.sayName();
+	IsabelSoares.sayName();
 	}
 }
