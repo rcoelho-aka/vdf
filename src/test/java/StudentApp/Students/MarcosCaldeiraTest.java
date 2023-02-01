0fce9a1d39b3d69069c01217e951ab26d6b3a79e
MarcosSWE <marcoshsec1992@gmail.com>
2023-02-01 19:22:53 +0000

    Jenkins22

---
+++ src/test/java/StudentApp/Students/MarcosCaldeiraTest.java
@@ -0,0 +1,24 @@
+package StudentApp.Students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MarcosCaldeira.class)
+class MarcosCaldeiraTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnRicardoCoelho() throws Exception {
+        mockMvc.perform(get("/marcos-caldeira"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'firstName':'Marcos', 'lastName':'Caldeira'}"));
+    }
+}
