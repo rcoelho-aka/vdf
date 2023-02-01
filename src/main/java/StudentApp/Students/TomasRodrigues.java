5a0e7e100b6faac272b2685ec3c8582a26247790
TomasRodrigues97 <tomasrodrigues97@gmail.com>
2023-02-01 18:43:47 +0000

    jenkins file

--- src/main/java/StudentApp/Students/TomasRodrigues.java
+++ src/main/java/StudentApp/Students/TomasRodrigues.java
@@ -1,14 +1,21 @@
 package StudentApp.Students;
 import StudentApp.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class TomasRodrigues {
 
-    public static void sayName() {
+    /*public static void sayName() {
     	Student student = new Student("Tomás", "Rodrigues");
     	System.out.print("My name is ");
     	student.sayFirstName();
     	System.out.print(" ");
         student.sayLastName();
         System.out.print("!");
+    }*/
+    Student student = new Student("Tomás", "Rodrigues");
+
+    @GetMapping("/tomas-rodrigues")
+    public Student index() {
+        return student;
     }
 }
