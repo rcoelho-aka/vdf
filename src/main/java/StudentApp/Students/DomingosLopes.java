105ff84b5175fcc4c8ae426a8d70160cf59af3cf
lopesdj <domingos.j.lopes@gmail.com>
2023-02-01 19:32:15 +0000

    Create DomingosLopes.java

---
+++ src/main/java/StudentApp/Students/DomingosLopes.java
@@ -0,0 +1,9 @@
+package StudentApp.Students;
+import StudentApp.Student;
+
+public class DomingosLopes {
+    public static void sayName() {
+        Student student = new Student("Domingos", "Lopes");
+        student.sayFullName();
+    }
+}
