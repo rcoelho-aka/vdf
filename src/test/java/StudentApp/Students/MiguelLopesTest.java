96f045a2cc2519765789296bbefd8bc71360db69
mlopes <miguel.lopes4@vodafone.com>
2023-02-01 19:33:34 +0000

    MiguelLopesTest.java

--- src/test/java/StudentApp/Students/MiguelLopesTest.java
+++ src/test/java/StudentApp/Students/MiguelLopesTest.java
@@ -9,7 +9,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(RicardoCoelho.class)
+@WebMvcTest(MiguelLopes.class)
 class MiguelLopesTest {
 
     @Autowired
