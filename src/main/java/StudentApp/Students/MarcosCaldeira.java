8b44cb43faba04eb4ddc6a822a554c862ef7527f
MarcosSWE <marcoshsec1992@gmail.com>
2023-02-01 19:17:02 +0000

    Jenkins21

--- src/main/java/StudentApp/Students/MarcosCaldeira.java
+++ src/main/java/StudentApp/Students/MarcosCaldeira.java
@@ -1,13 +1,15 @@
 package StudentApp.Students;
 import StudentApp.Student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MarcosCaldeira {
-	    public static void sayName() {
-		            Student student = new Student("Marcos", "Caldeira");
-			    System.out.print("My name is ");
-			    student.sayFirstName();
-			    System.out.print(" ");
-			    student.sayLastName();
-		            System.out.print("!");	    
-	    }
+	    Student student = new Student("Marcos", "Caldeira");
+
+	        @GetMapping("/marcos-caldeira")
+		        public Student index() {
+				                return student;
+			}
 }
