15c6fd397b01330ffb7b7514fcbbc218e1f5fea4
isabelSoares <isabel.srsoares@gmail.com>
2023-02-01 17:38:53 +0000

    Have fun coding!

--- src/main/java/StudentApp/Students/IsabelSoares.java
+++ src/main/java/StudentApp/Students/IsabelSoares.java
@@ -1,13 +1,20 @@
 package StudentApp.Students;
 import StudentApp.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class IsabelSoares {
-    public static void sayName() {
-        Student student = new Student("Isabel", "Soares");
-        System.out.print("My Name is ");
-        student.sayFirstName();
-        System.out.print(" ");
-        student.sayLastName();
-        System.out.print("!");
+    Student student = new Student("Isabel", "Soares");
+
+    @GetMapping("/isabel-soares")
+    public Student index() {
+        return student;
     }
+
+    @GetMapping("/isabel-soares/test")
+    public String test() {
+        return "test";
+    }
+
 }
