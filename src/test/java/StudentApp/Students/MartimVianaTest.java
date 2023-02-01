a9ec601f401bee46b811fcdeccfc89b8765552a5
MartimViana <martimviana98@gmail.com>
2023-02-01 17:57:16 +0000

    Add test for martim viana

---
+++ src/test/java/StudentApp/Students/MartimVianaTest.java
@@ -0,0 +1,24 @@
+package StudentApp.Students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RicardoCoelho.class)
+class RicardoCoelhoTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnRicardoCoelho() throws Exception {
+        mockMvc.perform(get("/martim-viana"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'firstName':'Martim', 'lastName':'Viana'}"));
+    }
+}
