c9535b20b7cabae3b4d5ab44ed63cd832d31b262
amgalves96 <amg.alves@campus.fct.unl.pt>
2023-02-01 17:36:40 +0000

    Added jenkins file

--- src/main/java/StudentApp/StudentController.java
+++ src/main/java/StudentApp/StudentController.java
@@ -10,5 +10,4 @@ public class StudentController {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
-
 }
--- src/main/java/StudentApp/Students/AfonsoAlves.java
+++ src/main/java/StudentApp/Students/AfonsoAlves.java
@@ -1,13 +1,19 @@
 package StudentApp.Students;
 import StudentApp.Student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class AfonsoAlves {
-    public static void sayName() {
-        Student student = new Student("Afonso", "Alves");
-        System.out.print("My name is ");
-        student.sayFirstName();
-        System.out.print(" ");
-        student.sayLastName();
-        System.out.print("!");
+    Student student = new Student("Afonso", "Alves");
+
+    @GetMapping("/afonso-alves")
+    public Student index() {
+        return student;
+    }
+
+    @GetMapping("/afonso-alves/test")
+    public String test() {
+        return "test";
     }
 }
