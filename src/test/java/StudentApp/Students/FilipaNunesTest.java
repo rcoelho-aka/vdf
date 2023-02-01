802a2a272be5a169e810fcd3db3df119c72ce17c
NunesF5 <filipa.nunes@vodafone.com>
2023-02-01 18:06:31 +0000

    step 9

--- src/test/java/StudentApp/Students/FilipaNunesTest.java
+++ src/test/java/StudentApp/Students/FilipaNunesTest.java
@@ -21,3 +21,27 @@ public class FilipaNunesTest {
                 .andExpect(content().json("{'firstName':'Filipa', 'lastName':'Nunes'}"));
     }
 }
+package StudentApp.Students;
+
+        import org.junit.jupiter.api.Test;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+        import org.springframework.test.web.servlet.MockMvc;
+
+        import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+        import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MartimViana.class)
+class MartimVianaTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnMartimViana() throws Exception {
+        mockMvc.perform(get("/martim-viana"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'firstName':'Martim', 'lastName':'Viana'}"));
+    }
+}
