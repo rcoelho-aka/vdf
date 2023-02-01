c58b28f2654455eb9b3436843325b4d8a1af0f5d
TomasRodrigues97 <tomasrodrigues97@gmail.com>
2023-02-01 19:03:56 +0000

    new TomasRodriguesTest file

---
+++ src/test/java/StudentApp/Students/TomasRodriguesTest.java
@@ -0,0 +1,23 @@
+package StudentApp.Students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(TomasRodrigues.class)
+public class TomasRodriguesTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnRicardoCoelho() throws Exception {
+        mockMvc.perform(get("/tomas-rodrigues"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'firstName':'Tomás', 'lastName':'Rodrigues'}"));
+    }
+}
