204fdd7dcc56521d4e43b76aaf84faeaa89b28c7
amgalves96 <amg.alves@campus.fct.unl.pt>
2023-02-02 11:01:06 +0000

    Added jenkins file

--- src/main/java/StudentApp/Students/AfonsoAlves.java
+++ src/main/java/StudentApp/Students/AfonsoAlves.java
@@ -4,6 +4,7 @@ import StudentApp.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AfonsoAlves {
     Student student = new Student("Afonso", "Alves");
 
