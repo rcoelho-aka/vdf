07c27965eb7ba90d335f028efc3fe4ce4156a794
TomasRodrigues97 <tomasrodrigues97@gmail.com>
2023-02-02 15:37:39 +0000

    pom file updated

--- src/main/java/StudentApp/Students/TomasRodrigues.java
+++ src/main/java/StudentApp/Students/TomasRodrigues.java
@@ -5,15 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TomasRodrigues {
-
-    /*public static void sayName() {
-    	Student student = new Student("Tomás", "Rodrigues");
-    	System.out.print("My name is ");
-    	student.sayFirstName();
-    	System.out.print(" ");
-        student.sayLastName();
-        System.out.print("!");
-    }*/
     Student student = new Student("Tomás", "Rodrigues");
 
     @GetMapping("/tomas-rodrigues")
