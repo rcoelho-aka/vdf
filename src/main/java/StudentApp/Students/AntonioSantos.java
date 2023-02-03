b09ce47bbfc710162355ad03c2a7c6463bf7ec1f
AntonioJVSantos <Antonio-j.santos@vodafone.com>
2023-02-03 13:15:29 +0000

    Testing SonarCLoud2

--- src/main/java/StudentApp/Students/AntonioSantos.java
+++ src/main/java/StudentApp/Students/AntonioSantos.java
@@ -7,9 +7,13 @@ import StudentApp.Student;
 @RestController
 public class AntonioSantos {
     Student student = new Student("Antonio", "Santos");
-    @GetMapping("/antonio-santos")    
+    @GetMapping("/antonio-santos")        
     public Student index(){
         return student;
+    }
+    @GetMapping("/antonio-santos/test")
+    public String test(){
+        return "test";
     }
     
 }
