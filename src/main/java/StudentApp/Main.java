b9b23b122a0a066cde159d73b89c00c6d42b399f
denysbr77 <denysbr@hotmail.com>
2023-02-15 14:34:14 +0000

    Commit do Step 2 - Main com : "Hello World! DenysFlorindo"

--- src/main/java/StudentApp/Main.java
+++ src/main/java/StudentApp/Main.java
@@ -3,11 +3,11 @@ import StudentApp.Students.DenysFlorindo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  //      System.out.println("Hello world!");
 
-/*        Student student = new Student("Denys", "Florindo");
+        Student student = new Student("Denys", "Florindo");
         System.out.println("Hello world!");
-        System.out.println("My name is " + student.firstName + student.lastName);
-*/
+        DenysFlorindo.sayName();
+
     }
 }
--- src/main/java/StudentApp/Students/DenysFlorindo.java
+++ src/main/java/StudentApp/Students/DenysFlorindo.java
@@ -3,10 +3,7 @@ import StudentApp.Student;
 public class DenysFlorindo {
 public static void sayName() {
         Student student = new Student("Denys", "Florindo");
-        System.out.print("My name is ");
         student.sayFirstName();
-        System.out.print(" ");
         student.sayLastName();
-        System.out.print("!");
     }
 }
